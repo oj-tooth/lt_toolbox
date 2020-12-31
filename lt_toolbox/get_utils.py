0a487e06c8405178c0ef9bc07762b76cc312e14d
Ollie Tooth <ollietooth@Ollies-MBP.broadband>
2020-12-31 18:35:51 +0000

    Removed get_seed() function.

--- lt_toolbox/get_utils.py
+++ lt_toolbox/get_utils.py
@@ -235,61 +235,6 @@ def get_duration(self):
     return duration
 
 ##############################################################################
-# Define get_seed() function.
-
-
-def get_seed(self):
-    """
-    Returns seeding level when particles are released (start of trajectory).
-
-    The seeding level, an integer between 1 and the total no. of seeding
-    levels, marks when a particle is released into the system and is
-    returned for all trajectories as a new ndarray.
-
-    Parameters
-    ----------
-    self : trajectories object
-        Trajectories object passed from trajectories class method.
-
-    Returns
-    -------
-    seed_level : ndarray
-        seeding level (integer) when each particle is released, with
-        dimension (traj).
-    """
-    # ----------------------------------------------------------
-    # Return start times for trajectories with get_start_time().
-    # ----------------------------------------------------------
-    start_time = get_start_time(self)
-
-    # -------------------------------------------------
-    # Determining particle seed_level from start_time.
-    # -------------------------------------------------
-    # Find the unique elements in start_time.
-    uniq = np.unique(start_time)
-
-    # Define seed_level as array of zeros with dimension (traj).
-    seed_level = np.zeros(len(start_time))
-
-    # Allocating common seed level for particles with equal
-    # start_time values.
-    # Initialising seed equal to 1.
-    seed = 1
-    for i in uniq:
-        seed_level[start_time == i] = seed
-        # Update seed.
-        seed += 1
-
-    # -------------------------------
-    # Setting ouput array data type.
-    # -------------------------------
-    # Setting seed_level type to int.
-    seed_level = seed_level.astype('int')
-
-    # Returning seed levels as ndarray, seed_level.
-    return seed_level
-
-##############################################################################
 # Define get_minmax() function.
 
 
