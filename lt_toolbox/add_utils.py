40c9f09d23067f4dc9d139af8190b5ba431625f4
Ollie Tooth <ollietooth@Ollies-MBP.broadband>
2021-01-03 20:59:06 +0000

    Added add_variable() function to be called from trajectories.py

--- lt_toolbox/add_utils.py
+++ lt_toolbox/add_utils.py
@@ -15,6 +15,7 @@
 # Importing relevant packages.
 
 import numpy as np
+import xarray as xr
 from get_utils import get_start_time
 
 ##############################################################################
@@ -112,3 +113,88 @@ def add_id(self):
 
     # Returning trajectory id as ndarray, traj_id.
     return traj_id
+
+##############################################################################
+# Define add_var() function.
+
+
+def add_var(self, data, attrs):
+    """
+    Adds an new variable to the trajectory DataSet.
+
+    The variable data must be provided as an ndarray with dimensions
+    (traj) / (obs) / (traj x obs) and the attributes provided as a
+    dictionary with keys - 'long_name', 'standard_name' and 'units' -
+    as a minimum.
+
+    Parameters
+    ----------
+    self : trajectories object
+        Trajectories object passed from trajectories class method.
+
+    Returns
+    -------
+    DataSet.
+        Complete trajectories, including new attribute variable to
+        be added.
+    """
+    # -------------------------------------------------
+    # Determining dimensions of new attribute variable.
+    # -------------------------------------------------
+    # Determine no. dimenions of data.
+    dims = np.ndim(data)
+
+    # Determine size of traj and obs dims.
+    traj = len(self.data.traj)
+    obs = len(self.data.obs)
+
+    # Defining variable name, var_name.
+    var_name = attrs['standard_name']
+
+    # -----------------------------------------
+    # Adding 1-dimensional variable to DataSet.
+    # -----------------------------------------
+    if dims == 1:
+        # Determine no. rows in data.
+        rows = np.shape(data)[0]
+
+        if rows == traj:
+            # Append variable DataArray to original DataSet.
+            self.data[var_name] = xr.DataArray(data, dims=["traj"])
+            # Adding attributes to new variable in DataArray.
+            self.data[var_name].attrs = attrs
+
+        elif rows == obs:
+            # Append variable DataArray to original DataSet.
+            self.data[var_name] = xr.DataArray(data, dims=["obs"])
+            # Adding attributes to new variable in DataArray.
+            self.data[var_name].attrs = attrs
+
+    # -----------------------------------------
+    # Adding 2-dimensional variable to DataSet.
+    # -----------------------------------------
+    elif dims == 2:
+        # Determine no. rows and cols in data.
+        rows = np.shape(data)[0]
+        cols = np.shape(data)[1]
+
+        # Raising ValueError if array shape differs from (traj x obs).
+        if rows != traj or cols != obs:
+            raise ValueError("data should have dimensions traj x obs")
+
+        # Append variable DataArray to original DataSet.
+        self.data[var_name] = xr.DataArray(data, dims=["traj", "obs"])
+        # Adding attributes to new variable in DataArray.
+        self.data[var_name].attrs = attrs
+
+    else:
+        # -------------------------------------------------
+        # Raise Error for incorrectly specified data array.
+        # -------------------------------------------------
+        # Raise ValueError if dimensions > 2.
+        raise ValueError("data should be provided as a 1- or 2-dimensional ndarray")
+
+    # ----------------------------------------------------
+    # Returning DataSet with new attribute variable added.
+    # ----------------------------------------------------
+    return self.data
