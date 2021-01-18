0bcb01f38e8260064e59b07cfe35adfc178b45f0
Ollie Tooth <ollietooth@Ollies-MBP.home>
2021-01-18 11:07:02 +0000

    Added find_polygon() method to return indices of trajectory points inside given geometry.

--- lt_toolbox/find_utils.py
+++ lt_toolbox/find_utils.py
@@ -15,72 +15,270 @@
 # Importing relevant packages.
 
 import numpy as np
+import pygeos
 
 ##############################################################################
-# Define find_traj() function.
+# Define find_between() method.
 
 
-def find_traj(self, find_type, variable, val='NaN', min_val='NaN', max_val='NaN'):
+def find_between(self, variable, min_val, max_val):
     """
-    Finding indices within trajectories using attribute variable.
+    Finding indices of points within trajectories where a specified
+    attribute variable takes a value between a specified min and
+    max.
 
-    Find returns the indices of trajectory points where the specified
-    attribute variable takes a value between a specified min and max
-    (including these values) or value equal to val.
+    Find returns the indices of trajectory points as a tuple of arrays
+    where the specified attribute variable takes a value between a
+    specified min and max (including these values).
+
+    When variable is specified as 'time' only the indices of observations
+    (obs) equal to and between the specified time-levels are returned for
+    all trajectories.
 
     Parameters
     ----------
-    self : trajectories object
-        Trajectories object passed from trajectories class method.
-    find_type : string
-        Name of find method to be used - 'between' or 'equal'.
     variable : string
         Name of the variable in the trajectories object.
-    val : numeric
-        Value variable should equal (using 'equal' method).
     min_val : numeric
-        Minimum value variable should equal or be greater than
-        (using 'between' method).
+        Minimum value variable should equal or be greater than.
     max_val : numeric
-        Maximum value variable should equal or be less than
-        (using 'between' method).
+        Maximum value variable should equal or be less than.
 
     Returns
     -------
-    DataSet.
-        Complete trajectories, including all attribute variables,
-        which meet the filter specification.
+    tuple
+        Indices of trajectory points where condition is satisfied.
+
+    Examples
+    --------
+    Find indices of trajectory points where Latitude is between
+    0 N - 20 N.
+
+    >>> trajectories.find_between('lat', 0, 20)
+
+    Finding indices of trajectory observations between two dates using
+    numpy datetime64.
+
+    >>> tmin = np.datetime64('2000-01-01')
+    >>> tmax = np.datetime64('2000-03-01')
+    >>> trajectories.find_between('time', tmin, tmax)
+
+    Finding indices of trajectory observations with time using numpy
+    timedelta64.
+
+    >>> tmin = np.timedelta64(0, 'D')
+    >>> tmax = np.timedelta64(90, 'D')
+    >>> trajectories.find_between('time', tmin, tmax)
     """
+    # -------------------
+    # Raising exceptions.
+    # -------------------
+    # Defining list of variables contained in data.
+    variables = list(self.data.variables)
 
-    # -----------------------------
-    # Routine for find_between().
-    # -----------------------------
-    if find_type == 'between':
+    if isinstance(variable, str) is False:
+        raise TypeError("variable must be specified as a string")
 
-        # ------------------------------------------------------------
-        # Finding indices for attribute variable between min and max.
-        # ------------------------------------------------------------
-        # Defining indices where trajectory points meeting conditions
-        # will be stored, use numpy vectorisation, np.where().
-        indices = np.where((self.data[variable].values <= max_val) & (self.data[variable].values >= min_val))
+    if variable not in variables:
+        raise ValueError("variable: \'" + variable + "\' not found in Dataset")
 
-        # Returning the tuple of arrays containing indices for satisfactory
-        # trajectory points.
-        return indices
+    # For non-time variables integers or floats only.
+    if variable != 'time':
+        if isinstance(min_val, (int, float)) is False:
+            raise TypeError("min must be specified as integer or float")
 
-    # ----------------------------
-    # Routine for find_equal().
-    # ----------------------------
+        if isinstance(max_val, (int, float)) is False:
+            raise TypeError("max must be specified as integer or float")
+    # For time variable numpy datetime64 or timedelta64 format only.
     else:
+        if isinstance(min_val, (np.datetime64, np.timedelta64)) is False:
+            raise TypeError("min must be specified as datetime64 or timedelta64")
 
-        # -----------------------------------------------------
-        # Finding indices for attribute variable equal to val.
-        # -----------------------------------------------------
+        if isinstance(max_val, (np.datetime64, np.timedelta64)) is False:
+            raise TypeError("max must be specified as datetime64 or timedelta64")
 
-        # Defining indices where trajectory points meeting conditions
-        # will be stored, use numpy vectorisation, np.where().
-        indices = np.where(self.data[variable].values == val)
+    # ------------------------------------------------------------
+    # Finding indices for attribute variable between min and max.
+    # ------------------------------------------------------------
 
-        # Returning the tuple of arrays containing indices for satisfactory
-        # trajectory points.
-        return indices
+    # Defining indices where trajectory points meeting conditions
+    # will be stored, use numpy vectorisation, np.where().
+    indices = np.where((self.data[variable].values <= max_val) & (self.data[variable].values >= min_val))
+
+    # Returning the tuple of arrays containing indices
+    # for satisfactory trajectory points.
+    return indices
+
+
+##############################################################################
+# Define find_equal() method.
+
+def find_equal(self, variable, val):
+    """
+    Finding indices of points within trajectories where a specified
+    attribute variable takes a value equal to a specified val.
+
+    Find returns the indices of trajectory points as a tuple of arrays
+    where the specified attribute variable takes a value equal to
+    specified val.
+
+    Parameters
+    ----------
+    variable : string
+        Name of the variable in the trajectories object.
+    val : numeric
+        Value variable should equal.
+
+    Returns
+    -------
+    tuple
+        Indices of trajectory points where condition is satisfied.
+
+    Examples
+    --------
+    Find indices of trajectory points where Latitude is equalt to
+    0 N.
+
+    >>> trajectories.find_equal('lat', 0)
+
+    Finding indices of trajectory observations for one date using
+    numpy datetime64.
+
+    >>> tval = np.datetime64('2000-03-01')
+    >>> trajectories.find_equal('time', tval)
+
+    Finding indices of trajectory observations for one time using
+    numpy timedelta64.
+
+    >>> tval = np.timedelta64(90, 'D')
+    >>> trajectories.find_equal('time', tval)
+    """
+    # -------------------
+    # Raising exceptions.
+    # -------------------
+    # Defining list of variables contained in data.
+    variables = list(self.data.variables)
+
+    if isinstance(variable, str) is False:
+        raise TypeError("variable must be specified as a string")
+
+    if variable not in variables:
+        raise ValueError("variable: \'" + variable + "\' not found in Dataset")
+
+    # For non-time variables integers or floats only.
+    if variable != 'time':
+        if isinstance(val, (int, float)) is False:
+            raise TypeError("val must be specified as integer or float")
+    # For time variable numpy datetime64 or timedelta64 format only.
+    else:
+        if isinstance(val, (np.datetime64, np.timedelta64)) is False:
+            raise TypeError("val must be specified as datetime64 or timedelta64")
+
+    # -----------------------------------------------------
+    # Finding indices for attribute variable equal to val.
+    # -----------------------------------------------------
+
+    # Defining indices where trajectory points meeting conditions
+    # will be stored, use numpy vectorisation, np.where().
+    indices = np.where(self.data[variable].values == val)
+
+    # Returning the tuple of arrays containing indices
+    # for satisfactory trajectory points.
+    return indices
+
+
+##############################################################################
+# Define find_polygon() method.
+
+def find_polygon(self, polygon):
+    """
+    Finding indices of trajectory points where a particle
+    is contained within a specified polygon.
+
+    Find returns the indices of trajectory points as a tuple of arrays.
+
+    Parameters
+    ----------
+    variable : string
+        Name of the variable in the trajectories object.
+    polygon : list
+    List of coordinates, specified as an ordered sequence of tuples
+    (Lon, Lat), representing the boundary of the polygon.
+
+    Returns
+    -------
+    tuple
+        Indices of trajectory points where condition is satisfied.
+
+    Examples
+    --------
+    Finding indices of trajectory points contained within a simple
+    polygon, square.
+
+    >>> square = [(-40, 30), (-40, 35), (-30, 35), (-30, 30), (-40, 30)]
+    >>> trajectories.find_polygon(square)
+    """
+    # -------------------
+    # Raising exceptions.
+    # -------------------
+    if isinstance(polygon, list) is False:
+        raise TypeError("polygon must be specified as a list of tuples")
+
+    # ------------------------------------------
+    # Defining Latitude and Longitude variables.
+    # ------------------------------------------
+    Lon = np.copy(self.data['lon'].values)
+    Lat = np.copy(self.data['lat'].values)
+
+    # Defining number of trajectories.
+    ntraj = np.shape(Lon)[0]
+    # Defining number of observations.
+    obs = np.shape(Lon)[1]
+
+    # -------------------------------------------
+    # Defining shapes from specified coordinates.
+    # -------------------------------------------
+    # Storing pygeos polygon, poly.
+    poly = pygeos.creation.polygons(polygon)
+
+    # ---------------------------------------------------------------
+    # Using pygeos to find trajectory points intersecting a polygon.
+    # ---------------------------------------------------------------
+    # Setting NaNs in Lat and Lon to a missing data value (must not be
+    # within range of coordinates -180E to 180E or -90N to 90N).
+    Lon[np.isnan(Lon)] = -99999
+    Lat[np.isnan(Lat)] = -99999
+
+    # Configuiring masks as an empty array (dims - traj x obs) of
+    # boolean type.
+    mask = np.zeros([ntraj, obs], dtype='bool')
+
+    # Iterating over all trajectories and defining a Point Collection
+    # with trajectory points.
+    for i in np.arange(ntraj):
+        # Storing Lats and Lons for trajectory i.
+        lon = Lon[i, :]
+        lat = Lat[i, :]
+
+        # Defining coordinates as transposed tuple of Lats and Lons.
+        coords = np.array((lon, lat)).T
+
+        # Defining points as a pygeos point collection.
+        trajectory_points = pygeos.creation.points(coords)
+
+        # Defining mask_traj evaluating if every trajectory point intersects
+        # the polygon.
+        mask_traj = pygeos.predicates.contains(poly, trajectory_points)
+        # Adding trajectory mask, mask_traj, to mask ndarray.
+        mask[i, :] = mask_traj
+
+    # --------------------------------------------------
+    # Finding indices for points intersecting a polygon.
+    # --------------------------------------------------
+    # Defining indices where trajectory points meeting conditions
+    # will be stored, use numpy vectorisation, np.where().
+    indices = np.where(mask == True)
+
+    # Returning the tuple of arrays containing indices
+    # for satisfactory trajectory points.
+    return indices
