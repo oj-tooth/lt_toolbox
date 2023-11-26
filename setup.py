2118db4e89443be0aaa62ea0411fd56eea371e3d
oj-tooth <oliver.tooth@env-res.ox.ac.uk>
2023-11-26 17:32:29 +0000

    Updated setup.py for pip installation

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name="lt_toolbox", version='1.0', packages=find_packages())
+setup(name='lt_toolbox',
+      version='0.1.0',
+      description='Lagrangian Trajectories Toolbox',
+      url='http://github.com/oj_tooth/lt_toolbox',
+      author='Ollie Tooth',
+      author_email='oliver.tooth@seh.ox.ac.uk',
+      license='MIT',
+      packages=['lt_toolbox'],
+      zip_safe=False)
