13e2e565cdfad7e735f3f7127f1a350428896972
oj-tooth <oliver.tooth@env-res.ox.ac.uk>
2023-12-30 17:48:21 +0000

    Added __init__ to create conversions module

---
+++ lt_toolbox/conversions/__init__.py
@@ -0,0 +1,15 @@
+##############################################################################
+# __init__.py
+#
+# Description:
+# __init__ file in order for conversions directory to be treated
+# as a package of modules.
+#
+# Last Edited:
+# 2023-12-30
+#
+# Created By:
+# Ollie Tooth
+#
+###############################################################################
+from lt_toolbox.conversions.export_utils import *
