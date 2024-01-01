9baa99f8d24e9b01897332423275e59e9d476a08
oj-tooth <oliver.tooth@env-res.ox.ac.uk>
2024-01-01 17:31:58 +0000

    Removed TrajStore import from lt_toolbox

--- lt_toolbox/__init__.py
+++ lt_toolbox/__init__.py
@@ -13,5 +13,4 @@
 #
 ###############################################################################
 from lt_toolbox.trajframe.trajframe import TrajFrame
-from lt_toolbox.trajstore.trajstore import TrajStore
 from lt_toolbox.trajarray.trajarray import TrajArray
