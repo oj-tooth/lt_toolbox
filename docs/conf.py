6d2037e6786ada372a2c8b8e3f8ddd95a20a50d8
oj-tooth <oliver.tooth@env-res.ox.ac.uk>
2024-01-03 12:17:17 +0000

    Updated logo for LT Toolbox site header

--- docs/conf.py
+++ docs/conf.py
@@ -67,11 +67,11 @@ html_theme = 'pydata_sphinx_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
-html_logo = "LT_Toolbox_Logo_Dark.png"
+html_logo = "LT_Toolbox_Logo_Icon.png"
 html_theme_options = {
     "logo": {
-        "image_light": "LT_Toolbox_Logo_Dark.png",
-        "image_dark": "LT_Toolbox_Logo_Dark.png",
+        "image_light": "LT_Toolbox_Logo_Icon.png",
+        "image_dark": "LT_Toolbox_Logo_Icon.png",
     }
 }
 
